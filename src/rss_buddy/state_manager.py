db48799474987af4d1d8e0a704bfdbadda755db3
Slava Dubovitsky <evil.mrfix@gmail.com>
2025-05-07 13:21:47 +0100

    StateManager: Add state validation.

--- src/rss_buddy/state_manager.py
+++ src/rss_buddy/state_manager.py
@@ -1,21 +1,34 @@
 import os
 import logging
-from typing import Dict, Optional
+from typing import Dict, List, Optional
 
 from pydantic import BaseModel
 
-from models import ItemGUID, ProcessedFeed
+from models import ItemGUID, ProcessedFeed, FeedCredentials
 
 # Original feed link.
 OriginalFeedLink = str
 
 class State(BaseModel):
-    last_processed_feeds: Dict[OriginalFeedLink, ProcessedFeed.ProcessingResult] # Last processed items for each feed
+    """
+    The state of the RSS Buddy.
+    """
+    class FeedData(BaseModel):
+        """
+        The data for a feed.
+        """
+        filter_criteria: Optional[str] # The filter criteria of the feed
+        processing_result: ProcessedFeed.ProcessingResult # The processing result of the feed
+
+    global_filter_criteria: Optional[str] = None # The global filter criteria
+    processed_feeds: Dict[OriginalFeedLink, FeedData] = {} # Processed items for each feed
 
 class StateManager:
     def __init__(
             self,
             file_path: str, # The path to load/save the state file
+            feed_credentials: List[FeedCredentials] , # The credentials of the feeds
+            global_filter_criteria: Optional[str] = None, # The global filter criteria
         ):
         """
         Initialize the state manager and load the state from the given path if present.
@@ -23,12 +36,58 @@ class StateManager:
 
         self._file_path = file_path
 
-        if os.path.exists(self._file_path):
-            with open(self._file_path, "r") as f:
-                self._state = State.model_validate_json(f.read())
+        # Load the state
+        if state := self._load_state(file_path):
+            self._state = self._validate_state(state, global_filter_criteria, feed_credentials)
         else:
-            logging.warning(f"State not found at \"{self._file_path}\". Starting with empty state.")
-            self._state = State(last_processed_feeds={})
+            self._state = State(global_filter_criteria=global_filter_criteria)
+
+    @classmethod
+    def _load_state(
+            cls,
+            file_path: str
+        ) -> Optional[State]:
+        """
+        Load the state from the given path.
+        """
+        # Load or create the state
+        if os.path.exists(file_path):
+            with open(file_path, "r") as f:
+                return State.model_validate_json(f.read())
+        
+        return None
+
+    @classmethod
+    def _validate_state(
+            cls,
+            state: State,
+            new_global_filter_criteria: Optional[str],
+            new_feed_credentials: List[FeedCredentials],
+        ) -> State:
+        """
+        Validate the state against the new data.
+
+        Returns:
+            Returns the original state if it is valid; otherwise, returns a new state containing only the data that remains valid for retrieving previous processing results.
+        """
+        # Check if the global filter criteria is valid
+        if new_global_filter_criteria != state.global_filter_criteria:
+            logging.warning(f"Global filter criteria has changed from \"{state.global_filter_criteria}\" to \"{new_global_filter_criteria}\". Starting with empty state.")
+            return State(global_filter_criteria=new_global_filter_criteria)
+        
+        # Check if the feeds metadata is valid
+        for feed_credentials in new_feed_credentials:
+            # Check if the feed is in the state
+            if feed_credentials.url not in state.processed_feeds:
+                logging.warning(f"Feed \"{feed_credentials.url}\" not found in state. Removing from state.")
+                del state.processed_feeds[feed_credentials.url]
+            # Check if the feed filter criteria is changed
+            if feed_credentials.filter_criteria != state.processed_feeds[feed_credentials.url].filter_criteria:
+                logging.warning(f"Feed filter criteria has changed for \"{feed_credentials.url}\". Removing from state.")
+                del state.processed_feeds[feed_credentials.url]
+        
+        # Return the original state
+        return state
 
     def item_previous_processing_result(
             self,
@@ -42,15 +101,15 @@ class StateManager:
             bool: True if the item has been processed, False otherwise.
             None: If the item has not been processed.
         """
-        feed_processing_result = self._state.last_processed_feeds.get(feed_link)
+        feed_processing_result = self._state.processed_feeds.get(feed_link)
         if not feed_processing_result:
             logging.info(f"Feed \"{feed_link}\" has not been previously processed.")
             return None
         
-        if item_guid in feed_processing_result.passed_item_guids:
+        if item_guid in feed_processing_result.processing_result.passed_item_guids:
             logging.info(f"Item \"{item_guid}\" has been previously processed and passed filter.")
             return True
-        elif item_guid in feed_processing_result.failed_item_guids:
+        elif item_guid in feed_processing_result.processing_result.failed_item_guids:
             logging.info(f"Item \"{item_guid}\" has been previously processed and failed filter.")
             return False
         else:
@@ -59,13 +118,16 @@ class StateManager:
 
     def update_state(
             self,
-            feed_link: OriginalFeedLink,
+            feed_credentials: FeedCredentials,
             processing_result: ProcessedFeed.ProcessingResult,
         ):
         """
         Update the state with the new processing result.
         """
-        self._state.last_processed_feeds[feed_link] = processing_result
+        self._state.processed_feeds[feed_credentials.url] = State.FeedData(
+            filter_criteria=feed_credentials.filter_criteria,
+            processing_result=processing_result,
+        )
 
     def write(self):
         """
--- tests/test_state_manager.py
+++ tests/test_state_manager.py
@@ -1,36 +1,66 @@
 import pytest
 from unittest.mock import patch, mock_open
-
+from typing import Optional
 from rss_buddy.state_manager import StateManager, State
-from models import ProcessedFeed, ItemGUID
+from models import ProcessedFeed, ItemGUID, FeedCredentials
 
-def feed_url():
-    return "https://example.com/feed"
+def feed_url(id: int):
+    return f"https://example.com/feed{id}"
+
+def filter_criteria(id: int):
+    return f"test-filter-criteria-{id}"
+
+def default_feed_data(id: int):
+    return State.FeedData(
+        filter_criteria=filter_criteria(id),
+        processing_result=ProcessedFeed.ProcessingResult(
+            passed_item_guids=[
+                ItemGUID(f"test-guid-1"),
+            ],
+            failed_item_guids=[
+                ItemGUID(f"test-guid-2"),
+            ],
+        ),
+    )
+
+def default_processed_feeds():
+    return {
+        feed_url(1): default_feed_data(1),
+        feed_url(2): default_feed_data(2),
+    }
 
 def default_state():
     return State(
-        last_processed_feeds={
-            feed_url(): ProcessedFeed.ProcessingResult(
-                passed_item_guids=[
-                    ItemGUID("test-guid-1"),
-                ],
-                failed_item_guids=[
-                    ItemGUID("test-guid-2"),
-                ],
-            )
-        }
+        global_filter_criteria="test-global-filter-criteria",
+        processed_feeds=default_processed_feeds(),
+    )
+
+def default_feed_credentials(
+        id: int,
+        criteria: Optional[str] = None,
+    ):
+    return FeedCredentials(
+        url=feed_url(id),
+        filter_criteria=criteria or filter_criteria(id),
+    )
+
+def empty_state_manager():
+    return StateManager(
+        file_path="",
+        feed_credentials=[],
+        global_filter_criteria="",
     )
 
 @pytest.mark.parametrize(
     "path_exists, expected_state",
     [
         (True, default_state()),
-        (False, State(last_processed_feeds={})),
+        (False, None),
     ]
 )
 @patch("builtins.open", new_callable=mock_open)
 @patch("os.path.exists", name="mock_exists")
-def test_state_manager_init(
+def test_state_manager_load_state(
         mock_exists,
         mock_open,
         path_exists,
@@ -38,13 +68,59 @@ def test_state_manager_init(
     ):
 
     mock_exists.return_value = path_exists
-    mock_open.return_value.read.return_value = expected_state.model_dump_json()
+    mock_open.return_value.read.return_value = expected_state.model_dump_json() if expected_state else None
 
-    state_manager = StateManager(
-        file_path="test_state.json"
+    state = StateManager._load_state(file_path="test_state.json")
+
+    assert state == expected_state
+
+@pytest.mark.parametrize(
+    "new_global_filter_criteria, new_feed_credentials, expected_state",
+    [
+        # No changes
+        (
+            default_state().global_filter_criteria,
+            [default_feed_credentials(1), default_feed_credentials(2)],
+            default_state(),
+        ),
+        # Global filter criteria changed
+        (
+            "new-global-filter-criteria",
+            [default_feed_credentials(1), default_feed_credentials(2)],
+            State(global_filter_criteria="new-global-filter-criteria"),
+        ),
+        # Feed credentials changed
+        (
+            default_state().global_filter_criteria,
+            [
+                FeedCredentials(
+                    url=feed_url(1),
+                    filter_criteria="new-filter-criteria-1",
+                ),
+                FeedCredentials(
+                    url=feed_url(2),
+                    filter_criteria="new-filter-criteria-2",
+                ),
+            ],
+            State(
+                global_filter_criteria=default_state().global_filter_criteria,
+                processed_feeds={},
+            ),
+        ),
+    ]
+)
+def test_state_manager_validate_state(
+        new_global_filter_criteria,
+        new_feed_credentials,
+        expected_state,
+    ):
+    state = StateManager._validate_state(
+        state=default_state(),
+        new_global_filter_criteria=new_global_filter_criteria,
+        new_feed_credentials=new_feed_credentials,
     )
 
-    assert state_manager._state == expected_state
+    assert state == expected_state
 
 @pytest.mark.parametrize(
     "item_guid, expected_result",
@@ -59,45 +135,55 @@ def test_state_manager_item_previous_processing_result(
         expected_result,
     ):
 
-    state_manager = StateManager(file_path="")
+    state_manager = empty_state_manager()
     state_manager._state = default_state()
 
     previous_processing_result = state_manager.item_previous_processing_result(
-        feed_url(), item_guid
+        feed_url(1), 
+        item_guid
     )
 
     assert previous_processing_result == expected_result
 
 @pytest.mark.parametrize(
-    "processing_result",
+    "feed_credentials, processing_result",
     [
-        ProcessedFeed.ProcessingResult(
-            passed_item_guids=[
-                ItemGUID("test-guid-1"),
-            ],
-            failed_item_guids=[
-                ItemGUID("test-guid-2"),
-            ],
+        (
+            default_feed_credentials(1),
+            default_feed_data(1).processing_result,
         ),
-        ProcessedFeed.ProcessingResult(
-            passed_item_guids=[],
-            failed_item_guids=[],
+        (
+            default_feed_credentials(2),
+            ProcessedFeed.ProcessingResult(
+                passed_item_guids=[],
+                failed_item_guids=[],
+            ),
         ),
     ]
 )
-def test_state_manager_update_state(processing_result):
-    state_manager = StateManager(file_path="")
+def test_state_manager_update_state(
+        feed_credentials,
+        processing_result,
+    ):
+    state_manager = empty_state_manager()
     state_manager._state = default_state()
 
-    state_manager.update_state(feed_url(), processing_result)
+    state_manager.update_state(
+        feed_credentials=feed_credentials,
+        processing_result=processing_result,
+    )
 
-    assert state_manager._state.last_processed_feeds[feed_url()] == processing_result
+    assert state_manager._state.processed_feeds[feed_credentials.url] == State.FeedData(
+        filter_criteria=feed_credentials.filter_criteria,
+        processing_result=processing_result,
+    )
 
 @patch("builtins.open", new_callable=mock_open)
 def test_state_manager_write(mock_open):
     test_file_path = "test_state.json"
 
-    state_manager = StateManager(file_path=test_file_path)
+    state_manager = empty_state_manager()
+    state_manager._file_path = test_file_path
     state_manager._state = default_state()
 
     state_manager.write()
